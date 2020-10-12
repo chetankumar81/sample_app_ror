c3f08fe79a738795f0d4dafe6c9295e049830125
chetankumar81 <chetanonline2020@gmail.com>
2020-10-12 17:52:52 +0530

    Finish user signup

---
+++ test/integration/users_signup_test.rb
@@ -0,0 +1,25 @@
+require 'test_helper'
+
+class UsersSignupTest < ActionDispatch::IntegrationTest
+  test 'invalid signup information' do
+    get signup_path
+    assert_no_difference 'User.count' do
+      post signup_path, params: { user: { name: '', email: 'user@invalid',
+                                          password: 'foo', password_confirmation: 'bar' } }
+    end
+    assert_template 'users/new'
+    assert_select 'div#error_explanation'
+    assert_select 'div.field_with_errors'
+  end
+
+  test 'valid signup information' do
+    get signup_path
+    assert_difference 'User.count',1 do
+      post signup_path, params: { user: { name: 'chetan', email: 'b@gmail.com', password: 'foo@123',
+                                          password_confirmation: 'foo@123' } }
+    end
+    follow_redirect!
+    assert_template 'users/show'
+    assert_not flash.nil?
+  end
+end
